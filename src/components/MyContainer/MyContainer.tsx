d32d60247f9d9a0cfe0c879e1f5b8b692aa7c147
shadow <lightshadow416@gmail.com>
2024-01-21 16:08:26 +0100

    [Satelite] component checkpoint {1}

--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -1,15 +1,16 @@
 import "./mycontainer.css";
 // components
-import { Navbar, Hero, About, Galaxy } from "../../components";
+import { Navbar, Hero, About, Galaxy, Satelite } from "../../components";
 export default function MyContainer() {
   return (
     <>
       <Navbar />
       <main>
         <Hero />
-        <div className="bg-black relative z-[1]">
+        <div className="bg-black relative z-[1] space-y-[10rem]">
           <About />
           <Galaxy />
+          <Satelite />
         </div>
       </main>
     </>
--- src/components/Satelite/Satelite.tsx
+++ src/components/Satelite/Satelite.tsx
@@ -1,4 +1,65 @@
 import "./satelite.css";
+// mui
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+// assets
+import satelite_img from "/satelite2.jpg";
 export default function Satelite() {
-  return <>Satelite</>;
+  return (
+    <>
+      <article className="flex  justify-between items-center">
+        <Details />
+        <section>
+          <img src={satelite_img} alt="img" className="h-[22rem]" />
+        </section>
+      </article>
+    </>
+  );
 }
+
+let Details = () => {
+  return (
+    <Card
+      component="section"
+      sx={{ color: "white", bgcolor: "transparent", pb: "2rem", pl: "1.2rem" }}
+      className="w-[45%] border-sky-800 border-b-2 border-l-2"
+    >
+      <Typography variant="h5" className="text-sky-800">
+        OUR MISSION
+      </Typography>
+      <CardHeader
+        sx={{ px: "0" }}
+        title={<Typography variant="h2">RAPIDCAST</Typography>}
+      />
+      <CardContent sx={{ pl: "0", pr: "6rem" }}>
+        <Typography variant="h6" component="p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos molestiae
+          reprehenderit expedita corporis, non doloremque. Consequatur
+          consectetur quisquam qui sunt. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Eos molestiae reprehenderit expedita corporis, non
+          doloremque. Consequatur consectetur quisquam qui sunt.
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ pl: "0" }}>
+        <Button
+          sx={{
+            color: "white",
+            px: "1.5rem",
+            py: "0.8rem",
+            fontSize: "1.2rem",
+            textTransform: "capitalize",
+          }}
+          className="!bg-blue-500"
+        >
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};

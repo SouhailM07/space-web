508758b07aad297acb88be993307acd98028f794
souhail <lightshadow416@gmail.com>
2024-02-26 00:30:52 +0100

    adding Helmet to the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import "./style/input.css";
 import "./App.css";
 // components
 import MyContainer from "./components/MyContainer/MyContainer";
+//
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="space web landing page" />
+        <meta
+          name="keywords"
+          content="responsive design, landing page, space web"
+        />
+        <meta name="author" content="Shadow" />
+      </Helmet>
       <MyContainer />
     </>
   );
